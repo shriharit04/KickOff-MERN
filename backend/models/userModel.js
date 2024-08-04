9773305b143aaa653a39af040e761bf5e291cec8
Shrihari <shriharit2004@gmail.com>
2024-08-04 21:55:39 +0530

    deploy microchange, installed bcryptjs

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const validator = require('validator')
 
 const Schema = mongoose.Schema
